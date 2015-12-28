f1e6aa16bb903299ca07116c7711a50a4d78e1b8
Chris Trawick <james.c.trawick@nielsen.com>
2015-12-28 17:18:41 -0500

    Added some javadoc

--- core/src/main/java/net/cultured/daoism/KeyedBatchUpdater.java
+++ core/src/main/java/net/cultured/daoism/KeyedBatchUpdater.java
@@ -3,9 +3,30 @@ package net.cultured.daoism;
 import java.util.Map;
 import java.util.function.Consumer;
 
+/**
+ * Updates batches of data by key, useful for re-keying data. Implementations of
+ * this interface take advantage of underlying data store efficiencies to update
+ * multiple records in a single step.
+ *
+ * @author chris
+ * @param <K>
+ *            the key type
+ * @param <T>
+ *            the data type
+ */
 public interface KeyedBatchUpdater<K, T> extends Consumer<Map<K, T>> {
+    /**
+     * Update multiple data records by key. The map keys are used to identify
+     * existing records, whose keys are then updated to those of the map values.
+     *
+     * @param data
+     *            the data records to update
+     */
     void updateManyByKey(Map<K, T> data);
 
+    /**
+     * Access {@link #updateManyByKey(Map)} as a functional interface method.
+     */
     @Override
     default void accept(final Map<K, T> t) {
         updateManyByKey(t);
